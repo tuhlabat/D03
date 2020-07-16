08decef22ed5a208a4cc9bfadda97423320b532e
tumelo hlabathe <tuhlabat@c5r4s5.wethinkcode.co.za>
2020-07-16 15:03:25 +0200

    modified file

--- ex01/ft_ultimate_ft.c
+++ ex01/ft_ultimate_ft.c
@@ -6,22 +6,15 @@
 /*   By: tuhlabat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/14 11:09:10 by tuhlabat          #+#    #+#             */
-/*   Updated: 2020/07/15 11:48:19 by tuhlabat         ###   ########.fr       */
+/*   Updated: 2020/07/16 12:13:48 by tuhlabat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
 
-void ft_putaddr(void *nbr);
-void ft_putnbr(int nbr);
 void ft_ultimate_ft(int *********nbr);
 
-int main(void){
-	int   a;
-	int  *nbr;
-    
-	a = 42;
-	nbr = &a;
-	return (0);
+{
+   	*********nbr = (42);
 }
